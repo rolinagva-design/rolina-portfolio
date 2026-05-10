2770107c0c716d9f3b222707e243b5d3aad243cb
Rolina <rolinagva@gmail.com>
2026-05-10 13:40:57 +1000

    Update App.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { AnimatePresence, motion, useScroll, useSpring } from "framer-motion";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 type Category =
   | "All"
